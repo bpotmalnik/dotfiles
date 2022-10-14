496ccb4098edfb72a98f7c5cd953a9a8b07d5456
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2022-10-14 19:46:34 +0100

    fix: deprecated formatting function

--- config/nvim/lua/bart/init.lua
+++ config/nvim/lua/bart/init.lua
@@ -3,5 +3,5 @@ require("bart.keymap")
 require("bart.packer")
 require("bart.telescope")
 
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
