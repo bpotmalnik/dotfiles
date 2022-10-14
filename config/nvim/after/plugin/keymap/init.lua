fbf9bcaa386b913ccb03c335b595dd53fb8e0f54
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2022-10-14 19:47:33 +0100

    feat: add same random keymaps

--- config/nvim/after/plugin/keymap/init.lua
+++ config/nvim/after/plugin/keymap/init.lua
@@ -12,13 +12,17 @@ nnoremap("<leader>pv", ":Ex<CR>")
 vnoremap("J", ":m '>+1<CR>gv=gv")
 vnoremap("K", ":m '>-2<CR>gv=gv")
 
+nnoremap("<leader>d", "\"_d")
+xnoremap("<leader>d", "\"_d")
 xnoremap("<leader>p", "\"_dp")
 
-
-nnoremap("<leader>y", "\"+y")
 vnoremap("<leader>y", "\"+y")
+nnoremap("<leader>Y", "\"+yg_")
+nnoremap("<leader>y", "\"+y")
+nnoremap("<leader>yy", "\"+yy")
 
-nmap("<leader>Y", "\"+Y")
+xnoremap("<Tab>", ">gv")
+xnoremap("<S-Tab>", "<gv")
 
 nnoremap("<leader>d", "\"_d")
 vnoremap("<leader>d", "\"_d")
