9a9833d38de93e1d81302c2b887eb890187c1c53
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2022-10-14 19:40:40 +0100

    fix: remove flavor from flutter run

--- config/nvim/after/plugin/flutter.lua
+++ config/nvim/after/plugin/flutter.lua
@@ -1,7 +1,6 @@
 local nnoremap = require("bart.keymap").nnoremap
 
-
-nnoremap("<leader>fs", ":FlutterRun --flavor development -t lib/main_development.dart<cr>")
+nnoremap("<leader>fs", ":FlutterRun<cr>")
 nnoremap("<leader>fd", ":FlutterDevices<cr>")
 nnoremap("<leader>fe", ":FlutterEmulators<cr>")
 nnoremap("<leader>fr", ":FlutterReload<cr>")
