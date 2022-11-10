3d4e3938b81e78cbb2fb687af45925167207ead9
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2022-11-10 18:31:12 +0000

    feat: add auto closing quickfix window

--- config/nvim/lua/bart/init.lua
+++ config/nvim/lua/bart/init.lua
@@ -5,3 +5,7 @@ require("bart.telescope")
 
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+vim.cmd [[autocmd FileType qf if getwininfo(win_getid())[0].loclist | execute 'nnoremap <silent><buffer><CR> :.ll <bar> lclose<cr>' | end ]]
+vim.cmd [[autocmd FileType qf if !getwininfo(win_getid())[0].loclist | execute 'nnoremap <silent><buffer><CR> :.cc <bar> cclose<cr>' | end ]]
+
